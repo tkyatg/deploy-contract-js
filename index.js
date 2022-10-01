cd16c4313b8b767179ab9ee83317e75cc28148e9
tkyatg <takuya.engineer911@gmail.com>
2022-10-01 16:26:23 +0900

    first commit

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
 fs = require("fs");
+solc = require("solc");
 
-const file = fs.readFileSync("./contract/base.sol").toString();
-console.log(file);
+const file = fs.readFileSync("initial.sol").toString();
+const input = {
+  language: "Solidity",
+  sources: {
+    "initial.sol": {
+      content: file,
+    },
+  },
+
+  settings: {
+    outputSelection: {
+      "*": {
+        "*": ["*"],
+      },
+    },
+  },
+};
+
+const output = JSON.parse(solc.compile(JSON.stringify(input)));
+console.log("Result : ", output); // solidity compile and json parse result.
+const abi = output.contracts["initial.sol"]["initial"].abi;
+console.log("ABI: ", abi); // need for access to contract
+const bytecode = output.contracts["initial.sol"]["initial"].evm.bytecode.object;
+console.log("BYTE_CODE: ", bytecode); // use for deploy contract
